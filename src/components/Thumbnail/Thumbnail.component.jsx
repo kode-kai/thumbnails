322c572ca902f94dc3066a62e86e33066e3964c6
Jesusfer2575 <ing.adrian94fdz@gmail.com>
2021-04-03 18:20:27 -0600

    feat: download button implemented

--- src/components/Thumbnail/Thumbnail.component.jsx
+++ src/components/Thumbnail/Thumbnail.component.jsx
@@ -8,9 +8,13 @@ const initialText = 'example';
 
 const Thumbnail = () => {
   const [subtitle, setSubtitle] = React.useState(initialText);
+  const [source, setSource] = React.useState('');
   const [isDark, setIsDark] = React.useState(false);
+  const [isDisabled, setIsDisabled] = React.useState(true);
+
   const containerRef = React.useRef(null);
   const imageRef = React.useRef(null);
+  const anchorRef = React.useRef(null);
 
   React.useEffect(() => { 
     (subtitle !== initialText) ? containerRef.current.style.display = '' : 
@@ -26,11 +30,20 @@ const Thumbnail = () => {
   
   const handleClick = () => {
     const input = containerRef.current;
+    setIsDisabled(false);
     domtoimage.toPng(input).then(imgData => {
       window.scrollTo(0,0);
       imageRef.current.src = imgData;
+      setSource(imgData);
       input.style.display = "none";
     });
+  }
+
+  const handleDownload = () => {
+    anchorRef.current.href = source;
+    anchorRef.current.download = 'Thumbnail.png';
+    document.body.appendChild(anchorRef.current);
+    anchorRef.current.click();
   }
 
   const handleReload = () => {
@@ -43,11 +56,15 @@ const Thumbnail = () => {
       <div>
         <Img src={PlaceholderImage} ref={imageRef} id="myImage" alt="canvas"/>
         <br/>
-        <input placeholder="Enter your text here" onChange={handleChange} type="text"/>
+        <input placeholder="Enter your topic here" onChange={handleChange} type="text"/>
         <input type="button" value="Invert Theme" onClick={handleTheme}/>
         <br />
         <input type="button" value="Capture" onClick={handleClick}/>
         <input type="button" value="Reload" onClick={handleReload}/>
+        <input type="button" value="Download" onClick={handleDownload} disabled={isDisabled}/>
+        <div>
+          <a ref={anchorRef} href='#' style={{display: 'none'}} />
+        </div>
         <br />
       </div>
       <div>
