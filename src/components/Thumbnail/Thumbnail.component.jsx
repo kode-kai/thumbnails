bf7b2af8426bbb1c85911bd6776431e24b103478
Antonio Feregrino <antonio.feregrino@gmail.com>
2021-04-05 18:47:28 +0100

    fix: set proper href

--- src/components/Thumbnail/Thumbnail.component.jsx
+++ src/components/Thumbnail/Thumbnail.component.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import domtoimage from 'dom-to-image';
+import React from "react";
+import domtoimage from "dom-to-image";
 
-import PlaceholderImage from '../../assets/placeholder.png';
-import { Div, Img } from './Thumbnail.styled';
+import PlaceholderImage from "../../assets/placeholder.png";
+import { Div, Img } from "./Thumbnail.styled";
 
-const initialText = 'example';
+const initialText = "example";
 
 const Thumbnail = () => {
   const [subtitle, setSubtitle] = React.useState(initialText);
-  const [source, setSource] = React.useState('');
+  const [source, setSource] = React.useState("");
   const [isDark, setIsDark] = React.useState(false);
   const [isDisabled, setIsDisabled] = React.useState(true);
 
@@ -16,65 +16,81 @@ const Thumbnail = () => {
   const imageRef = React.useRef(null);
   const anchorRef = React.useRef(null);
 
-  React.useEffect(() => { 
-    (subtitle !== initialText) ? containerRef.current.style.display = '' : 
-      containerRef.current.style.display = 'none'; 
+  React.useEffect(() => {
+    subtitle !== initialText
+      ? (containerRef.current.style.display = "")
+      : (containerRef.current.style.display = "none");
   }, [subtitle]);
 
   const handleChange = (e) => {
     const text = e.target.value;
     setSubtitle(text);
-  }
+  };
 
-  const handleTheme = () => setIsDark(isDark => !isDark);
-  
+  const handleTheme = () => setIsDark((isDark) => !isDark);
+
   const handleClick = () => {
     const input = containerRef.current;
     setIsDisabled(false);
-    domtoimage.toPng(input).then(imgData => {
-      window.scrollTo(0,0);
+    domtoimage.toPng(input).then((imgData) => {
+      window.scrollTo(0, 0);
       imageRef.current.src = imgData;
       setSource(imgData);
       input.style.display = "none";
     });
-  }
+  };
 
   const handleDownload = () => {
     anchorRef.current.href = source;
-    anchorRef.current.download = 'Thumbnail.png';
+    anchorRef.current.download = "Thumbnail.png";
     document.body.appendChild(anchorRef.current);
     anchorRef.current.click();
-  }
+  };
 
   const handleReload = () => {
     window.location.reload();
     return false;
-  }
+  };
 
   return (
     <>
       <div>
-        <Img src={PlaceholderImage} ref={imageRef} id="myImage" alt="canvas"/>
-        <br/>
-        <input placeholder="Enter your topic here" onChange={handleChange} type="text"/>
-        <input type="button" value="Invert Theme" onClick={handleTheme}/>
+        <Img src={PlaceholderImage} ref={imageRef} id="myImage" alt="canvas" />
         <br />
-        <input type="button" value="Capture" onClick={handleClick}/>
-        <input type="button" value="Reload" onClick={handleReload}/>
-        <input type="button" value="Download" onClick={handleDownload} disabled={isDisabled}/>
+        <input
+          placeholder="Enter your topic here"
+          onChange={handleChange}
+          type="text"
+        />
+        <input type="button" value="Invert Theme" onClick={handleTheme} />
+        <br />
+        <input type="button" value="Capture" onClick={handleClick} />
+        <input type="button" value="Reload" onClick={handleReload} />
+        <input
+          type="button"
+          value="Download"
+          onClick={handleDownload}
+          disabled={isDisabled}
+        />
         <div>
-          <a ref={anchorRef} href='#' style={{display: 'none'}} />
+          <a
+            ref={anchorRef}
+            href="https://kodekai.com/thumbnails"
+            style={{ display: "none" }}
+          >
+            Download thumbnail
+          </a>
         </div>
         <br />
       </div>
       <div>
         <Div ref={containerRef} id="div" isDark={isDark}>
-          <h1 style={{fontSize: 400}}>kode kai</h1>
-          <h2 style={{fontSize: 150}}>{subtitle}</h2>
+          <h1 style={{ fontSize: 400 }}>kode kai</h1>
+          <h2 style={{ fontSize: 150 }}>{subtitle}</h2>
         </Div>
       </div>
     </>
-  )
+  );
 };
 
 export default Thumbnail;
